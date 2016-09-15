7fa1784ae172fb0cd01c96e4bc48425c519db078
Mak3r Space <aravind@mak3r.space>
2016-09-15 12:41:38 +0530

    Index page
    scripts - background music - jump to scene

---
+++ Scripts/Loadonclick.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loadonclick : MonoBehaviour {
+
+	public GameObject LoadingImage;
+
+	public void LoadScene(int level)
+	{
+		LoadingImage.SetActive (true);
+		Application.LoadLevel(level);
+
+	}
+}
