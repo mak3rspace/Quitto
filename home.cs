98efdc433e208ab11bd68dafd6659deb90eecf34
Mak3r Space <aravind@mak3r.space>
2016-09-16 21:15:12 +0530

    Models and Prefabs
    index page models
    new fonts
    scenes

---
+++ home.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class home : MonoBehaviour {
+	public Canvas credits;
+	public Canvas info;
+	public Canvas tap;
+	public Canvas main;
+	public GameObject a;
+	public GameObject b;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	public void hom () {
+		GameController b = FindObjectOfType<GameController> ();
+		b.ResetGame();
+		main.enabled = true;
+		info.enabled = false;
+		tap.enabled = false;
+		credits.enabled = false;
+		a.SetActive(false);
+	}
+}
---
+++ tap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class tap : MonoBehaviour {
+	public Canvas credits;
+	public Canvas info;
+	public Canvas taps;
+	public Canvas main;
+	public GameObject a;
+	public Canvas gameOverCanvas;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	public void tapp () {
+		
+		main.enabled = false;
+		info.enabled = false;
+		taps.enabled = true;
+		credits.enabled = false;
+		a.SetActive(true);
+		gameOverCanvas.enabled = false;
+	}
+}
